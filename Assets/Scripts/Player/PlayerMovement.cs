502f91887f80a3c4a55b5fba017c36f3abea3965
fes <fesss32@yandex.ru>
2021-03-18 23:13:11 +0500

    Init

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _speed;
+
+    private Rigidbody _rigidbody;
+    private bool _isGrounded = true;
+    private Vector3 _normalizeDirection= new Vector3(1,0,0);
+    private Vector3 _gravity = new Vector3(0, -9.8f, 0);
+
+    private void OnEnable()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+
+        if (Input.GetKey(KeyCode.Space) & _isGrounded)
+            Jump();    
+    }
+
+    private void Move()
+    {
+        _rigidbody.velocity = new Vector3(_speed, _rigidbody.velocity.y);
+    }
+
+    private void Jump()
+    {
+        _rigidbody.AddForce(Vector3.up * _jumpForce);
+        _isGrounded = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Ground>())    
+            _isGrounded = true;       
+    }
+} 
